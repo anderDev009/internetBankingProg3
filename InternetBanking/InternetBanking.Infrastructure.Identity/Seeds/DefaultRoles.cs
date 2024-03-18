728ca474dfa237088a01988744c15d7c1172524a
HanselRm <20220897@itla.edu.do>
2024-03-18 18:33:31 -0400

    Referencia de Identity to  Application, Enuns y Seeds de default roles

---
+++ InternetBanking/InternetBanking.Infrastructure.Identity/Seeds/DefaultRoles.cs
@@ -0,0 +1,18 @@
+﻿using InternetBanking.Core.Application.Enums;
+using InternetBanking.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace InternetBanking.Infrastructure.Identity.Seeds
+{
+    public static class DefaultRoles
+    {
+        //Metodo para crear los DefaultAdmin role
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Creacion de roles por defecto
+            await roleManager.CreateAsync(new IdentityRole(Roles.Administrator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Client.ToString()));
+        }
+    }
+}
