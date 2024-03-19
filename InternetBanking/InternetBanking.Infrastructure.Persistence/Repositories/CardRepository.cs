c1d727630eab72d2e12ee911c38784a2b2cae0c6
Ander <richardanderdev009@gmail.com>
2024-03-19 17:10:11 -0400

    Repositorio de cuentas de banco, tajetas y prestamos creados

---
+++ InternetBanking/InternetBanking.Infrastructure.Persistence/Repositories/CardRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using InternetBanking.Core.Application.Interfaces.Repositories;
+using InternetBanking.Core.Domain.Entities;
+using InternetBanking.Infrastructure.Persistence.Context;
+
+namespace InternetBanking.Infrastructure.Persistence.Repositories
+{
+    public class CardRepository : BaseRepository<Card>, ICardRepository
+    {
+        private readonly InternetBankingContext _ctx;
+        public CardRepository(InternetBankingContext ctx) : base(ctx) 
+        {
+            _ctx = ctx;            
+        }
+    }
+}
