0c1cdbda43c7cd8b2adeb15fe8d1e393172ed9d2
HanselRm <20220897@itla.edu.do>
2024-03-18 23:13:59 -0400

    Autentication responde and request dtos

---
+++ InternetBanking/InternetBanking.Infrastructure.Identity/Services/AccountService.cs
@@ -0,0 +1,21 @@
+﻿using InternetBanking.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using InternetBanking.Core.Application.Interfaces.Service;
+
+
+namespace InternetBanking.Infrastructure.Identity.Services
+{
+    public class AccountService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailServices _emailService;
+
+        public AccountService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IEmailServices emailService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailService = emailService;
+        }
+    }
+}
