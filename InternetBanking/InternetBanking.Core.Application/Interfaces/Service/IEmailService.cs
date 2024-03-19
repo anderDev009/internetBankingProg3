acca2ee4c78debace5869b99f20921b6801eee79
HanselRm <20220897@itla.edu.do>
2024-03-18 22:01:11 -0400

    Interface de Email

---
+++ InternetBanking/InternetBanking.Core.Application/Interfaces/Service/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using InternetBanking.Core.Application.Dtos.Email;
+
+namespace InternetBanking.Core.Application.Interfaces.Service
+{
+    public interface IEmailServices
+    {
+        Task sendAsync(EmailRequest request);
+    }
+}
