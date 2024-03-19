5a21b09fe967f409fb0b5e6ed1b67f1b0f8633d5
Ander <richardanderdev009@gmail.com>
2024-03-19 17:09:11 -0400

    Registro de servicios en la capa de aplicacion

--- InternetBanking/InternetBanking.Infrastructure.Persistence/ServiceRegistration.cs
+++ InternetBanking/InternetBanking.Infrastructure.Persistence/ServiceRegistration.cs
@@ -1,5 +1,7 @@
 ﻿
+using InternetBanking.Core.Application.Interfaces.Repositories;
 using InternetBanking.Infrastructure.Persistence.Context;
+using InternetBanking.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,8 +12,23 @@ namespace InternetBanking.Infrastructure.Persistence
     {
         public static void AddInfrastructureLayer(this IServiceCollection services,IConfiguration configuration)
         {
+
+            #region context
             services.AddDbContext<InternetBankingContext>(options => options.UseSqlServer(configuration.GetConnectionString("default"),
                 m => m.MigrationsAssembly(typeof(InternetBankingContext).Assembly.FullName)));
+            #endregion
+            #region Services
+            #region Repositorio Cuenta de banco
+            services.AddTransient<IBankAccountRepository, BankAccountRepository>();
+            #endregion
+            #region Repositorio card
+            services.AddTransient<ICardRepository, CardRepository>();
+            #endregion
+            #region Repositorio de prestamos
+            services.AddTransient<IloanRepository, LoanRepository>();
+            #endregion
+            #endregion
+
         }
     }
 }
