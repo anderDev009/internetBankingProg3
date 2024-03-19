283bac7c7f55facc67e70a46a9d1a6625b26a133
HanselRm <20220897@itla.edu.do>
2024-03-19 00:46:37 -0400

    AccountServices y ViewModel User

---
+++ InternetBanking/InternetBanking.Core.Application/ViewModels/Users/SaveUserViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace InternetBanking.Core.Application.ViewModels.Users
+{
+    public class SaveUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar el nombre")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar el apellido")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar la cédula")]
+        [RegularExpression("(^[0-9]+$)", ErrorMessage = "Solo se permiten números")]
+        [StringLength(11, ErrorMessage = "Debe tener  11 numeros", MinimumLength = 11)]
+        public string CardIdentificantion { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar un correo")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar un nombre de usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar una contraseña")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Las contraseñas no coiciden")]
+        [Required(ErrorMessage = "Debe colocar una contraseña")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar el tipo de usuario")]
+        public string TypeUser { get; set; }
+        public double? Amount { get; set; }
+        public bool HasError { get; set; }
+        public string Error { get; set; }
+
+    }
+}
