40f3cda1fa120cdf504aef26949821ead655709d
Ander <richardanderdev009@gmail.com>
2024-03-23 13:50:44 -0400

    Cambios transferencia bancaria

--- InternetBanking/WebApp/Controllers/PayController.cs
+++ InternetBanking/WebApp/Controllers/PayController.cs
@@ -52,5 +52,41 @@ namespace WebApp.Controllers
             return RedirectToRoute(new { controller = "Pay", action = "Express" });
 
         }
+
+        public async Task<IActionResult> Transfer()
+        {
+            var user = _contextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+            var accounts = await _bankAccountService.GetAccountsByIdUserAsync(user.Id);
+            ViewBag.Accounts = accounts;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(SavePayExpressViewModel vm)
+        {
+            if(!ModelState.IsValid)
+            {
+                ViewBag.Error = "Campos invalidos";
+                return View("Transfer");
+            }
+            //comprobamos que no sea a la misma cuenta
+            if(vm.AccountNumber == vm.IdAccountPaid)
+            {
+                ViewBag.Error = "No puedes transferir dinero a la misma cuenta.";
+                return View("Transfer");
+            }
+            try
+            {
+                await _payExpressService.SaveAsync(vm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return RedirectToRoute(new { controller = "Pay", action = "Transfer" });
+            }
+            ViewBag.Success = "Transferencia completada";
+            return View("Transfer");
+        }
+            
     }
 }
