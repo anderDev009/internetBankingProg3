dbe4626a5b45d4799a427b3f7a8c02883374a98c
HanselRm <20220897@itla.edu.do>
2024-03-23 12:21:35 -0400

    actions para prestamopay

--- InternetBanking/WebApp/Controllers/PayController.cs
+++ InternetBanking/WebApp/Controllers/PayController.cs
@@ -3,6 +3,7 @@ using InternetBanking.Core.Application.Helpers;
 using InternetBanking.Core.Application.Interfaces.Service;
 using InternetBanking.Core.Application.ViewModels.PayCard;
 using InternetBanking.Core.Application.ViewModels.PayExpress;
+using InternetBanking.Core.Application.ViewModels.PayLoan;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,19 +12,24 @@ namespace WebApp.Controllers
     [Authorize(Roles = "Client")]
     public class PayController : Controller
     {
-        private readonly IPayExpressService _payExpressService;
         private readonly IHttpContextAccessor _contextAccessor;
-        private readonly IBankAccountService _bankAccountService;
+        private readonly IPayExpressService _payExpressService;
+        private readonly IPayLoanService _payLoanServices;
         private readonly IPayCardService _payCardService;
+        private readonly IBankAccountService _bankAccountService;
         private readonly ICardService _cardService;
+        private readonly ILoanService _loanService;
         public PayController(IPayExpressService payExpressService, IHttpContextAccessor contextAccessor,
-            IBankAccountService bankAccountService, ICardService cardAccountService, IPayCardService payCardService)
+            IBankAccountService bankAccountService, ICardService cardAccountService, IPayCardService payCardService,
+            IPayLoanService payLoanServices, ILoanService loanService)
         {
-            _payExpressService = payExpressService;
             _contextAccessor = contextAccessor;
+            _payExpressService = payExpressService;
+            _payLoanServices = payLoanServices;
+            _payCardService = payCardService;
             _bankAccountService = bankAccountService;
             _cardService = cardAccountService;
-            _payCardService = payCardService;
+            _loanService = loanService;
         }
         public IActionResult Index()
         {
@@ -72,13 +78,45 @@ namespace WebApp.Controllers
         public async Task<IActionResult> Card(SavePayCardViewModel vm)
         {
             if (ModelState["Amount"].Errors.Any() || ModelState["IdAccountPaid"].Errors.Any()
-                || ModelState["IdAccountPaid"].Errors.Any())
+                || ModelState["IdCard"].Errors.Any())
             {
                 return RedirectToRoute(new { controller = "Pay", action = "Card" });
             }
             try
             {
                 await _payCardService.SaveAsync(vm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return RedirectToRoute(new { controller = "Pay", action = "Card" });
+            }
+
+            return RedirectToRoute(new { controller = "Pay", action = "Card" });
+
+        }
+
+        //pagos loan
+        public async Task<IActionResult> Loan()
+        {
+            var user = _contextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+            var loans = await _loanService.GetAllAsync();
+            var banks = await _bankAccountService.GetAllAsync();
+            ViewBag.loans = loans.FindAll(a => a.IdUser == user.Id);
+            ViewBag.banks = banks.FindAll(a => a.IdUser == user.Id);
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Loan(SavePayLoanViewModel vm)
+        {
+            if (ModelState["Amount"].Errors.Any() || ModelState["IdAccountPaid"].Errors.Any()
+                || ModelState["IdLoan"].Errors.Any())
+            {
+                return RedirectToRoute(new { controller = "Pay", action = "Card" });
+            }
+            try
+            {
+                await _payLoanServices.SaveAsync(vm);
             }
             catch (Exception ex)
             {
